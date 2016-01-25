982b8acf6b32bda4b027f5ed1322a1f5b3bf10ce
Kyle Derrick and Mao Thygesen <kderrick1988@gmail.com>
2016-01-25 10:56:42 -0800

    add jQuery for user behavior

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,24 @@ function Place (country, landmark, month, year, notes) {
 Place.prototype.placeDate = function () {
   return this.country + " " + this.month + ", " + this.year ;
 }
+
+$(document).ready(function(){
+  $('form#new-place').submit(function(event){
+    event.preventDefault();
+
+    var inputCountry = $("input#new-country").val();
+    var inputLandmark = $("input#new-landmark").val();
+    var inputMonth = $("input#new-month").val();
+    var inputYear = $("input#new-year").val();
+    var inputNotes =$("input#new-notes").val();
+    var newPlace = new Place(inputCountry, inputLandmark, inputMonth, inputYear, inputNotes);
+
+    $("ul#places").append("<li><span class = 'listPlace'>" + newPlace.placeDate() + "</span></li>");
+
+    $("input#new-country").val("");
+    $("input#new-landmark").val("");
+    $("input#new-month").val("");
+    $("input#new-year").val("");
+    $("input#new-notes").val("");
+  });
+});
