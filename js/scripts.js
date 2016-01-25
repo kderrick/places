6a47b71b2412d43d690d5e4028b81263860921c2
Kyle Derrick and Mao Thygesen <maothygesen@gmail.com>
2016-01-25 10:23:00 -0800

    add placeDate method

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
-function Place (country, landmark, month, notes) {
+function Place (country, landmark, month, year, notes) {
   this.country = country;
   this.landmark = landmark;
   this.month = month;
+  this.year = year;
   this.notes = notes;
 };
+
+Place.prototype.placeDate = function () {
+  return this.country + " " + this.month + ", " + this.year ;
+}
--- specs/specs.js
+++ specs/specs.js
@@ -1,10 +1,14 @@
 describe("Place", function(){
   it("will create a new place with the given properties", function() {
-    var testPlace = new Place("France", "Eiffel Tower", "March", "It was fun!");
+    var testPlace = new Place("France", "Eiffel Tower", "March", "2004", "It was fun!");
     expect(testPlace.country).to.equal("France");
     expect(testPlace.landmark).to.equal("Eiffel Tower");
     expect(testPlace.month).to.equal("March");
+    expect(testPlace.year).to.equal("2004");
     expect(testPlace.notes).to.equal("It was fun!");
-  
+  });
+  it("create placeDate method that concatnates country, month, year", function(){
+    var testPlace = new Place("France", "Eiffel Tower", "March", "2004", "It was fun!");
+    expect(testPlace.placeDate()).to.equal("France March, 2004");
   });
 });
