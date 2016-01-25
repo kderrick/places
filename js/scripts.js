b232601c44504be8466922837692af55656395f4
Kyle Derrick and Mao Thygesen <kderrick1988@gmail.com>
2016-01-25 13:22:07 -0800

    complete project

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 function Place (country, landmark, month, year, notes) {
-  this.country = country;
+  this.country = country.toUpperCase();
   this.landmark = landmark;
   this.month = month;
   this.year = year;
   this.notes = notes;
 };
+
 
 Place.prototype.placeDate = function () {
   return this.country + " " + this.month + ", " + this.year ;
@@ -23,11 +24,8 @@ $(document).ready(function(){
 
     $("ul#places").append("<li><span class = 'listPlace'>" + newPlace.placeDate() + "</span></li>");
 
-    $("input#new-country").val("");
-    $("input#new-landmark").val("");
-    $("input#new-month").val("");
-    $("input#new-year").val("");
-    $("input#new-notes").val("");
+    $("form input").val("");
+
 
     $(".listPlace").last().click(function(){
       $("#show-places").show();
--- specs/specs.js
+++ specs/specs.js
@@ -1,14 +1,22 @@
 describe("Place", function(){
   it("will create a new place with the given properties", function() {
-    var testPlace = new Place("France", "Eiffel Tower", "March", "2004", "It was fun!");
-    expect(testPlace.country).to.equal("France");
+    var testPlace = new Place("FRANCE", "Eiffel Tower", "March", "2004", "It was fun!");
+    expect(testPlace.country).to.equal("FRANCE");
     expect(testPlace.landmark).to.equal("Eiffel Tower");
     expect(testPlace.month).to.equal("March");
     expect(testPlace.year).to.equal("2004");
     expect(testPlace.notes).to.equal("It was fun!");
   });
+
   it("create placeDate method that concatnates country, month, year", function(){
+    var testPlace = new Place("FRANCE", "Eiffel Tower", "March", "2004", "It was fun!");
+    expect(testPlace.placeDate()).to.equal("FRANCE March, 2004");
+  });
+});
+
+describe("Upper", function(){
+  it("will take country name and make it uppercase", function(){
     var testPlace = new Place("France", "Eiffel Tower", "March", "2004", "It was fun!");
-    expect(testPlace.placeDate()).to.equal("France March, 2004");
+    expect(testPlace.country).to.equal("FRANCE")
   });
 });
